c1550d6f9147a7a05701be522f215c5528c10054
Jan Henning Thorsen <jhthorsen@cpan.org>
2013-09-09 20:34:22 +0200

    Add Mojolicious::Plugin::LinkEmbedder

---
+++ lib/Mojolicious/Plugin/LinkEmbedder.pm
@@ -0,0 +1,74 @@
+package Mojolicious::Plugin::LinkEmbedder;
+
+=head1 NAME
+
+Mojolicious::Plugin::LinkEmbedder - Convert a URL to embedded content
+
+=head1 VERSION
+
+0.01
+
+=head1 DESCRIPTION
+
+This module can transform a URL to an iframe, image or other embeddable
+content.
+
+=head1 SYNOPSIS
+
+  use Mojolicious::Lite;
+  plugin 'LinkEmbedder';
+
+  get '/embed' => sub {
+    my $self = shift;
+    my $link = $self->embed_link($self->param('url'));
+
+    $self->respond_to(
+      json => {
+        json => {
+          is_media => $link->is_media,
+          is_movie => $link->is_movie,
+          media_id => $link->media_id,
+          url => $link->url->to_string,
+        },
+      },
+      any => { text => "$link" },
+    );
+  };
+
+  app->start;
+
+=head1 SUPPORTED LINKS
+
+=cut
+
+use Mojo::Base 'Mojolicious::Plugin';
+use Mojolicious::Plugin::LinkEmbedder::Default;
+
+our $VERSION = '0.01';
+
+=head1 METHODS
+
+=head2 register
+
+Will register the L</embed_link> helper which creates new objects from
+L<Mojolicious::Plugin::LinkEmbedder::Default>.
+
+=cut
+
+sub register {
+  my($self, $app, $config) = @_;
+
+  $app->helper(embed_link => sub {
+    Mojolicious::Plugin::LinkEmbedder::Default->new($_[1]);
+  });
+}
+
+=head1 AUTHOR
+
+Jan Henning Thorsen - C<jhthorsen@cpan.org>
+
+Marcus Ramberg - C<mramberg@cpan.org>
+
+=cut
+
+1;
