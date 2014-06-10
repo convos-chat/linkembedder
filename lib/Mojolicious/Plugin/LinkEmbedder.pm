3155cd53ce367009dcd6fbfdc6367044ec44bd38
Jan Henning Thorsen <jhthorsen@cpan.org>
2014-06-10 17:16:41 +0200

    Released version 0.05
           * Fix embedding Text::GistGithub

--- lib/Mojolicious/Plugin/LinkEmbedder.pm
+++ lib/Mojolicious/Plugin/LinkEmbedder.pm
@@ -6,7 +6,7 @@ Mojolicious::Plugin::LinkEmbedder - Convert a URL to embedded content
 
 =head1 VERSION
 
-0.04
+0.05
 
 =head1 DESCRIPTION
 
@@ -81,7 +81,7 @@ use Mojo::UserAgent;
 use Mojolicious::Plugin::LinkEmbedder::Link;
 use constant DEBUG => $ENV{MOJO_LINKEMBEDDER_DEBUG} || 0;
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 my $LOADER = Mojo::Loader->new;
 
 has _ua => sub { Mojo::UserAgent->new };
