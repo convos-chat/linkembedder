68c1922fe4e3a4ef3428552ec8e04a569b4c28f8
Jan Henning Thorsen <jhthorsen@cpan.org>
2015-01-15 23:08:26 +0100

    Released version 0.18
           * Fix escaping markup when embedding HTML pages #13
             This includes Github links
           * Will only learn meta information from HTML if response is success #14
           * Video will have controls #12
           * Twitter cards will embed media
           * Remove cache_cb attribute
           * Add caching to predefined handler
             Marked caching as EXPERIMENTAL

--- lib/Mojolicious/Plugin/LinkEmbedder.pm
+++ lib/Mojolicious/Plugin/LinkEmbedder.pm
@@ -6,7 +6,7 @@ Mojolicious::Plugin::LinkEmbedder - Convert a URL to embedded content
 
 =head1 VERSION
 
-0.17
+0.18
 
 =head1 DESCRIPTION
 
@@ -140,7 +140,7 @@ use Mojo::UserAgent;
 use Mojolicious::Plugin::LinkEmbedder::Link;
 use constant DEBUG => $ENV{MOJO_LINKEMBEDDER_DEBUG} || 0;
 
-our $VERSION = '0.17';
+our $VERSION = '0.18';
 
 has _ua => sub { Mojo::UserAgent->new(max_redirects => 3) };
 
