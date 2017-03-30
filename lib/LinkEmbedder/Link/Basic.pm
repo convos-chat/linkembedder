a9f57a0413bfbeae765458b086b572293e0277ef
Jan Henning Thorsen <jhthorsen@cpan.org>
2017-03-30 18:40:14 +0200

    Add LinkEmbedder::Link::Basic

---
+++ lib/LinkEmbedder/Link/Basic.pm
@@ -0,0 +1,89 @@
+package LinkEmbedder::Link::Basic;
+use Mojo::Base 'LinkEmbedder::Link';
+
+use Mojo::Util 'trim';
+
+my $PHOTO_RE = qr!\.(?:jpg|png|gif)\b!i;
+my $VIDEO_RE = qr!\.(?:mpg|mpeg|mov|mp4|ogv)\b!i;
+
+has description => '';
+
+sub learn {
+  my ($self, $cb) = @_;
+  my $url = $self->url;
+  my $type = $url =~ $PHOTO_RE ? 'photo' : $url =~ $VIDEO_RE ? 'video' : 'link';
+
+  $self->type($type);
+
+  # Need to learn more from an http request
+  if ($type eq 'link') {
+    if ($cb) {
+      $self->ua->get($url => sub { $self->_learn($_[1])->$cb });
+    }
+    else {
+      $self->_learn($self->ua->get($url));
+    }
+  }
+  else {
+    $self->$cb if $cb;
+  }
+
+  return $self;
+}
+
+sub _learn {
+  my ($self, $tx) = @_;
+  my $ct = $tx->res->headers->content_type || '';
+
+  $self->type('photo')                                if $ct =~ m!^image/!;
+  $self->type('video')                                if $ct =~ m!^video/!;
+  $self->type('rich')                                 if $ct =~ m!^text/plain!;
+  $self->type('rich')->_learn_from_dom($tx->res->dom) if $ct =~ m!^text/html!;
+
+  return $self;
+}
+
+sub _learn_from_dom {
+  my ($self, $dom) = @_;
+
+  $self->_val(author_name => $dom, '[itemprop="author"] [itemprop="name"]');
+  $self->_val(author_url  => $dom, '[itemprop="author"] [itemprop="email"]');
+
+  $self->_val(
+    description => $dom,
+    'meta[property="og:description"]', 'meta[name="twitter:description"]', 'meta[name="description"]'
+  );
+
+  $self->_val(
+    thumbnail_url => $dom,
+    'meta[property="og:image"]', 'meta[property="og:image:url"]', 'meta[name="twitter:image"]',
+  );
+
+  $self->_val(thumbnail_height => $dom, 'meta[property="og:image:height"]');
+  $self->_val(thumbnail_width  => $dom, 'meta[property="og:image:width"]');
+  $self->_val(provider_name    => $dom, 'meta[property="og:site_name"]', 'meta[property="twitter:site"]');
+
+  $self->_val(title => $dom, 'meta[property="og:title"]', 'meta[name="twitter:title"]', 'title');
+
+  $self->_val(url => $dom, 'meta[property="og:url"]', 'meta[name="twitter:url"]');
+}
+
+sub _val {
+  my ($self, $attr, $dom, @sel) = @_;
+
+  for (@sel) {
+    my $e = $dom->at($_) or next;
+    my $val = trim($e->{content} || $e->{value} || $e->{href} || $e->text || '') or next;
+    $val = Mojo::URL->new($val) if $attr eq 'url';
+    return $self->$attr($val);
+  }
+}
+
+1;
+
+__DATA__
+@@ rich.html.ep
+<div class="card le-card le-<%= $l->type %>">
+  <h3><%= $l->title %></h3>
+  <p><%= $l->description %></p>
+</div>
---
+++ t/basic.t
@@ -0,0 +1,65 @@
+use Mojo::Base -strict;
+use Test::More;
+use LinkEmbedder;
+
+plan skip_all => 'TEST_ONLINE=1' unless $ENV{TEST_ONLINE};
+
+my $link;
+my $embedder = LinkEmbedder->new;
+
+$link = $embedder->get('http://thorsen.pm/blog/');
+is ref($link), 'LinkEmbedder::Link::Basic', 'LinkEmbedder::Link::Basic';
+
+is_deeply $link->TO_JSON,
+  {
+  cache_age     => 0,
+  html          => thorsen_html(),
+  provider_name => 'Thorsen',
+  title         => 'jhthorsen - blog',
+  type          => 'rich',
+  url           => 'http://thorsen.pm/blog/',
+  provider_url  => 'http://thorsen.pm/',
+  version       => '1.0'
+  },
+  'json for thorsen.pm';
+
+$link = $embedder->get(
+  'http://www.aftenposten.no/kultur/Kunstig-intelligens-ma-ikke-lenger-trenes-av-mennesker-617794b.html');
+
+is_deeply $link->TO_JSON,
+  {
+  author_name      => 'Per Kristian Bjørkeng',
+  author_url       => 'mailto:per.kristian.bjorkeng@aftenposten.no',
+  cache_age        => 0,
+  html             => twitter_html(),
+  provider_name    => 'Aftenposten',
+  provider_url     => 'http://www.aftenposten.no/',
+  thumbnail_height => 810,
+  thumbnail_url    => 'https://ap.mnocdn.no/images/ae53dc79-22e3-41da-b64b-16ec78f42a1a?fit=crop&q=80&w=1440',
+  thumbnail_width  => 1440,
+  title            => 'Google har skapt kunstig intelligens som trener seg selv',
+  type             => 'rich',
+  url              => 'http://www.aftenposten.no/article/ap-617794b.html',
+  version          => '1.0'
+  },
+  'json for twitter';
+
+done_testing;
+
+sub thorsen_html {
+  return <<'HERE';
+<div class="card le-card le-rich">
+  <h3>jhthorsen - blog</h3>
+  <p></p>
+</div>
+HERE
+}
+
+sub twitter_html {
+  return <<'HERE';
+<div class="card le-card le-rich">
+  <h3>Google har skapt kunstig intelligens som trener seg selv</h3>
+  <p>– Vi tror det vil komme kunstig intelligens på nivå med mennesker før eller siden, men det er veldig vanskelig å si om det blir 10 eller 50 år til, sier forsker.</p>
+</div>
+HERE
+}
