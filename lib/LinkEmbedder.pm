532d1a1b29a4e2791f2c06b0f768b60d326c549b
Jan Henning Thorsen <jhthorsen@cpan.org>
2018-09-26 01:22:22 +0900

    Released version 1.00
     - Compatible with Mojolicious 8.00
     - Converted from delay to promises

--- lib/LinkEmbedder.pm
+++ lib/LinkEmbedder.pm
@@ -9,7 +9,7 @@ use Mojo::UserAgent;
 
 use constant DEBUG => $ENV{LINK_EMBEDDER_DEBUG} || 0;
 
-our $VERSION = '0.05';
+our $VERSION = '1.00';
 
 my $PROTOCOL_RE = qr!^(\w+):\w+!i;    # Examples: mail:, spotify:, ...
 
